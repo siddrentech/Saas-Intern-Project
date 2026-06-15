4854d9eb99c30d5d2ef1de3ae056faf9af4d1438
siddrentech <siddjs23@gmail.com>
2026-06-15 08:12:44 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-10">
+      <h1 className="text-4xl font-bold">SwiftSell SaaS Portal POC</h1>
+      <p className="mt-4 text-lg">
+        My first Next.js app is running.
+      </p>
+    </main>
+  );
+}
